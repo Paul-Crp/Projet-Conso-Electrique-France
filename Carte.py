368a58d3471a35900128f7eac2772f96d1ef4420
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-11-10 21:05:55 +0100

    Update Carte.py
    Début de la création de la carte. J'ai utilisé des marqueurs dont on pourra adapter la tailler et la couleur en fonction de la consommation électrique de la ville.
    Il faut trouver un moyen d'obtenir les coordonnées des villes et ajouter les consommations pour chacune.

--- Carte.py
+++ Carte.py
@@ -1,26 +1,29 @@
-import folium
+import folium, branca
 
-# Création d'une carte
+VILLES = ['Aix-en-Provence', "Angers", "Bordeaux", "Brest", "Clermont-Ferrand", "Dijon", "Le Havre", "Le Mans", "Lille", "Lyon", "Marseille", "Montpellier", "Nantes", "Nice", "Nîmes", "Paris", "Reims", "Rennes", "Saint-Denis", "Saint-Étienne", "Toulon", "Toulouse", "Villeurbanne"]
+
+#Coordonnées des villes
+LATS = []
+LONGS = []
+
+#Consommation éléctrique
+CONSO = []
+
+#Création carte
 fmap = folium.Map(location=[47, 2], tiles='OpenStreetMap', zoom_start=6)
 
-#display(fmap)
+cm = branca.colormap.LinearColormap(['blue','red'], vmin=min(CONSO), vmax=max(CONSO))
+fmap.add_child(cm)
 
-# Ajout d'un marqueur
-#folium.Marker([55.8716693, 9.8839912],
-#              popup='VIA University College',
-#              icon=folium.Icon(color='green')).add_to(fmap)
+#Marqueurs
+for lat, lng, color in zip(LATS, LONGS, CONSO) :
+    folium.CircleMarker(
+        location = [lat, lng],
+        radius = 10,
+        color = cm(color),
+        fill = True,
+        fill_color = cm(color),
+        fill_opacity = 0.6
+    ).add_to(fmap)
 
-
-# Ajout d'une ligne brisée définie à partir de 5 points
-#points = [
-#  (55.6713808, 12.4533972),
-#  (55.4013094, 11.2062387),
-#  (55.3311408, 10.6322608),
-#  (55.7148992, 9.3734841),
-#  (55.8716693, 9.8839912)
-#]
-
-#folium.PolyLine(points, color='blue', weight=2.5, opacity=0.8).add_to(fmap)
-
-# Génération du fichier HTML contenant la carte
-fmap.save('via.html')
+fmap.save('Carte.html')
