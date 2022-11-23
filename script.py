6fa765f414e6eb1b5f54116a590df4ebdec0faa1
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-11-23 13:00:21 +0100

    Create script.py
    Script de lancement du module

---
+++ script.py
@@ -0,0 +1,7 @@
+import Interactive_Map as IM
+import pandas as pd
+
+geo=IM.get_geo(IM.Load_geo().save_as_df())
+df = IM.final_data(IM.Load_db().save_as_df(),geo)
+
+IM.legend(df,IM.carte(df,geo)).save('Carte.html')
