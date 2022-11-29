cec0c02671560a244a775b7ca77f7db72712d4f8
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-11-29 14:00:39 +0100

    Update get_data.py
    Corrections de bug

--- inter_map/preprocess/get_data.py
+++ inter_map/preprocess/get_data.py
@@ -1,8 +1,7 @@
 import  pandas as pd
 import numpy as np
-from Interactive_Map.preprocess import url_conso
 
-def get_conso(url_conso):
+def get_conso(df_conso):
     """
     Cette fonction extrait du data set de consommation la consommation annuelle moyenne des foyers par ville
 
@@ -11,9 +10,8 @@ def get_conso(url_conso):
     df_conso : (dataframe) dat set contenant les consommations électriques par foyers
     log_scale : (boolean)
     """
-    df_conso = pd.read_csv(url_conso, on_bad_lines='warn', sep=";")
-    df_conso.drop(df.columns[[1, 2, 3, 4, 5, 6, 7, 9, 14, 15]], axis=1, inplace=True)
-    df_conso.to_csv('../data/TableauTraité.csv', index=False)
+    df_conso.drop(df_conso.columns[[1, 2, 3, 4, 5, 6, 7, 9, 14, 15]], axis=1, inplace=True)
+    df_conso.to_csv('inter_map/data/TableauTraité.csv', index=False)
 
     df_conso["Nom de la commune"]=df_conso["Nom de la commune"].str.lower()
     df1 = df_conso.groupby(['Nom de la commune','Année'])[[df_conso.columns[5]]].aggregate(lambda x: x.mean()).reset_index()
