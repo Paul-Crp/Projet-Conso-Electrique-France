33a85ac5e167d7197cf9d805b2df8643a39a49ab
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-12-01 20:10:06 +0100

    Update __init__.py

--- inter_map/__init__.py
+++ inter_map/__init__.py
@@ -3,4 +3,4 @@ __version__ = "0.0.1"
 from .io.Load_geo import Load_geo
 from .io.Load_db import Load_db
 from .vis.Carte import carte, legend
-from .preprocess.get_data import get_geo, get_conso, final_data
+from .preprocess.get_data import get_geo, get_conso, final_data, tri
