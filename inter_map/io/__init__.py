a28596f6d79d2f7350cf470645ff80b8fbc291ec
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-12-01 19:58:40 +0100

    Update __init__.py
    Nouveau fichier geson : contour des départements

--- inter_map/io/__init__.py
+++ inter_map/io/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-url_geo = "https://static.data.gouv.fr/resources/contours-des-communes-de-france-simplifie-avec-regions-et-departement-doutre-mer-rapproches/20220219-095144/a-com2022.json"
-path_target_geo = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","data","commf2020.json")
+url_geo = "https://www.data.gouv.fr/fr/datasets/r/90b9341a-e1f7-4d75-a73c-bbc010c7feeb"
+path_target_geo = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","data","departements.json")
 
 url_db="https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B"
 path_target_db = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..","data","conso.csv")
