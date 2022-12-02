ff8ff599285ce1a39d7ee50b5c510001561c1d4d
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-12-02 11:13:05 +0100

    Update Load_geo.py
    Update doc

--- inter_map/io/Load_geo.py
+++ inter_map/io/Load_geo.py
@@ -6,13 +6,13 @@ from inter_map.io import url_geo, path_target_geo
 
 class Load_geo:
     """
-    This class download the geographic data set
+    Cette classe télécharge le fichier au format json décvrivant les contours des départements français.
 
     Parameters :
     ---------------
 
-    url : (string) path to the data set
-    target_name : (string) local path where data is saved
+    url : (string) adresse url du jeu de données
+    target_name : (string) chemin local où le jeu de données est enregistrer
     """
     def __init__(self, url=url_geo, target_name=path_target_geo):
         path, fname = os.path.split(path_target_geo)
@@ -21,11 +21,7 @@ class Load_geo:
     @staticmethod
     def save_as_df():
         """
-        A documentation
-        
-        .. code:: python
-            >>> import inter_map
-            >>> Load_geo.save_as_df()
+        Téléchargement des données dans un dataframe.
         """
         df_geo=gpd.read_file(path_target_geo)
 
