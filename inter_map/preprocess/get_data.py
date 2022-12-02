57e5bdab9808dded4e5e649cf88fd139aabb9e93
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-12-02 15:42:41 +0100

    pep8

--- inter_map/preprocess/get_data.py
+++ inter_map/preprocess/get_data.py
@@ -1,6 +1,7 @@
-import  pandas as pd
+import pandas as pd
 import numpy as np
 from inter_map.preprocess import Villes
+
 
 def tri(df_conso):
     """
@@ -12,28 +13,30 @@ def tri(df_conso):
     df_conso : (dataframe) dataset contenant les consommation électrique par foyers
     """
 
-    df_conso.drop(df_conso.columns[[1, 2, 3, 4, 5, 6, 9, 14, 15]], axis=1, inplace=True)
-    
-    Dept=[]
+    df_conso.drop(
+        df_conso.columns[[1, 2, 3, 4, 5, 6, 9, 14, 15]], axis=1, inplace=True)
+
+    Dept = []
     for i in df_conso['Code INSEE de la commune']:
-        i=int(i/1000)
-        if i<10:
+        i = int(i/1000)
+        if i < 10:
             Dept.append(f'{i}'.zfill(2))
         else:
             Dept.append(f'{i}')
 
-    df_conso.insert(1,'Département', Dept)
+    df_conso.insert(1, 'Département', Dept)
     df_conso.drop('Code INSEE de la commune', axis=1, inplace=True)
-    
+
     df_conso["Nom de la commune"] = df_conso["Nom de la commune"].str.lower()
-    df=pd.DataFrame()
+    df = pd.DataFrame()
     for i in Villes:
-        df1 = df_conso[df_conso['Nom de la commune']==i]
-        df = pd.concat([df,df1],ignore_index=True)
+        df1 = df_conso[df_conso['Nom de la commune'] == i]
+        df = pd.concat([df, df1], ignore_index=True)
 
     df.to_csv('inter_map/data/TableauTraité.csv', index=False)
-    
-    return(df)
+
+    return (df)
+
 
 def get_conso(df_conso):
     """
@@ -45,34 +48,35 @@ def get_conso(df_conso):
     """
     df = tri(df_conso)
 
-    df = df.groupby(['Département','Nom de la commune','Année'])[[df.columns[6]]].aggregate(lambda x: x.max()).reset_index()
-    df = df.groupby(['Département','Nom de la commune'])[[df.columns[3]]].aggregate(lambda x : x.mean())).reset_index()
-    
+    df = df.groupby(['Département', 'Nom de la commune', 'Année'])[
+                    [df.columns[6]]].aggregate(lambda x: x.max()).reset_index()
+    df = df.groupby(['Département', 'Nom de la commune'])[[df.columns[3]]].aggregate(lambda x: x.mean()).reset_index()
+
     return df
 
 def get_geo(df_geo):
     """
     Cette fonction extrait les numéros, nom et contours des département de France.
-    
+
     Paramètres :
     --------------
     df_geo : (dataframe) contours des départements français au format json
     """
-    geo=df_geo.loc[:,('code','nom','geometry')]
-    
-    return(geo)
+    geo = df_geo.loc[:, ('code', 'nom', 'geometry')]
+
+    return (geo)
 
 def final_data(df_conso, df_geo):
     """
     Cette fonction fusionne le dataframe des contours des départements français trié et le dataframe de la consommation électrique trié.
-    
+
     Paramètres :
     -------------
     df_conso : (dataframe) données de consommation triées
     df_geo : (dataframe) contours des départements français trié
     """
-    df_final = get_geo(df_geo).merge(get_conso(df_conso), left_on='conde', right_on='Département',how='outer')
-    df_final=df_final[~df_final['geometry'].isna()]
-    df_final=df_final.dropna()
-    
-    return(df_final)
+    df_final= get_geo(df_geo).merge(get_conso(df_conso), left_on='conde', right_on='Département', how='outer')
+    df_final= df_final[~df_final['geometry'].isna()]
+    df_final= df_final.dropna()
+
+    return (df_final)
