9e383babf6db6a2916b0d3b91c7d4a3d724a93cb
Thamara Renoir <113884805+Thamara97@users.noreply.github.com>
2022-12-05 15:16:41 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,4 +5,4 @@ geo = IM.get_geo(IM.Load_geo().save_as_df())
 df = IM.final_data(IM.Load_db().save_as_df(), geo)
 
 os.makedirs('Carte', exist_ok=True)
-IM.legend(df, IM.carte(df, geo)).save('Carte/Carte.html')
+IM.legend(df, IM.carte(df, geo)).save('Rendus/Carte.html')
