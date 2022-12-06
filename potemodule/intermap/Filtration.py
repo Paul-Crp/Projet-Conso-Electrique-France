122c27e52fa69e5e32e1365a8ed4bf1ad6f8a581
Paul-Crp <Paulcrespin159@gmail.com>
2022-12-06 20:18:17 +0100

    Doc Filtration.py et Graphique.py

--- potemodule/intermap/Filtration.py
+++ potemodule/intermap/Filtration.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
 def Filtrer():
+
+    '''
+
+    Cette fonction va filtrer les données du csv pour ne garder que les données qui nous intéressent. Elle retourne un dictionnaire qui contient les données que l'on va utiliser pour la carte.
+
+    '''
+    
     ChoixVilles = ['Aix-en-Provence', "Angers", "Bordeaux", "Brest", "Clermont-Ferrand", "Dijon", "Le Havre", "Le Mans", "Lille", "Lyon", "Marseille", "Montpellier", "Nantes", "Nice", "Nîmes", "Paris", "Reims", "Rennes", "Saint-Denis", "Saint-Etienne", "Saint-Étienne", "Toulon", "Toulouse", "Villeurbanne"]
     df = pd.read_csv('./potemodule/intermap/data/conso.csv', on_bad_lines='warn', sep=";",low_memory=False)
     df.drop(df.columns[[1, 2, 3, 4, 5, 6, 7, 9, 14, 15]], axis=1, inplace=True)
 
-    #On a le la liste des villes les plus grandes de France, on retire les donneés qui ne nous intéressent pas dans le csv.
+    #On a le la liste des villes les plus grandes de France, on retire les données qui ne nous intéressent pas dans le csv.
 
     dff = df[df['Nom de la commune'].str.lower().isin([x.lower() for x in ChoixVilles])]
 
@@ -28,7 +35,7 @@ def Filtrer():
     del d['Total-Saint-Etienne2021']
     del d['Total-Saint-Étienne2018']
 
-    #Les données de Saint-Etienne en 2018 sont sans accents, mais celle des autres années sont avec.
+    # Les données de Saint-Etienne en 2018 sont sans accents, mais celle des autres années sont avec.
     # Par simplicité, on a mis un doublon avec et sans accent dans la liste des villes, mais cela pose problème pour le dictionnaire.
     # On supprime donc les données vides avec parcimonie.
 
