b8d4c8f3e0b8090e785789d07a0527e4c033882a
Paul-Crp <Paulcrespin159@gmail.com>
2022-12-06 03:44:12 +0100

    Etienne? accent? oui et non

--- potemodule/intermap/Filtration.py
+++ potemodule/intermap/Filtration.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def Filtrer():
-    ChoixVilles = ['Aix-en-Provence', "Angers", "Bordeaux", "Brest", "Clermont-Ferrand", "Dijon", "Le Havre", "Le Mans", "Lille", "Lyon", "Marseille", "Montpellier", "Nantes", "Nice", "Nîmes", "Paris", "Reims", "Rennes", "Saint-Denis", "Saint-Étienne", "Toulon", "Toulouse", "Villeurbanne"]
+    ChoixVilles = ['Aix-en-Provence', "Angers", "Bordeaux", "Brest", "Clermont-Ferrand", "Dijon", "Le Havre", "Le Mans", "Lille", "Lyon", "Marseille", "Montpellier", "Nantes", "Nice", "Nîmes", "Paris", "Reims", "Rennes", "Saint-Denis", "Saint-Etienne", "Saint-Étienne", "Toulon", "Toulouse", "Villeurbanne"]
     df = pd.read_csv('./potemodule/intermap/data/conso.csv', on_bad_lines='warn', sep=";",low_memory=False)
     df.drop(df.columns[[1, 2, 3, 4, 5, 6, 7, 9, 14, 15]], axis=1, inplace=True)
 
@@ -14,5 +14,10 @@ def Filtrer():
             df1 = dff[dff['Année']==a]
             df2 = df1[df1['Nom de la commune'].str.lower().isin([i.lower()])]
             d["Total-{0}".format(i+str(a))] = round(df2['Consommation annuelle totale de l\'adresse (MWh)'].sum(),3)
+
+    del d['Total-Saint-Etienne2019']
+    del d['Total-Saint-Etienne2020']
+    del d['Total-Saint-Etienne2021']
+    del d['Total-Saint-Étienne2018']
 
     return(d)
